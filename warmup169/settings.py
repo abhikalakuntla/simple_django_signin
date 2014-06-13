3deb5e448f3e70d982370fb116e266179a89d16f
abhikalakuntla <abhi.kalakuntla@gmail.com>
2014-06-13 16:46:16 -0700

    initial

--- warmup169/settings.py
+++ warmup169/settings.py
@@ -65,28 +65,14 @@ WSGI_APPLICATION = 'warmup169.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-if(PRODUCTION == True):
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.sqlite3',
-			'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-		}
+
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	}
-
-	# Parse database configuration from $DATABASE_URL
-	import dj_database_url
-	DATABASES['default'] =  dj_database_url.config()
-
-
-	# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-else:
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.sqlite3',
-			'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-		}
-	}
+}
 
 """
 DATABASES = {
