4da5baea6129e351ba76c24f619311583683d01b
abhi kalakuntla <abhi.kalakuntla@gmail.com>
2014-06-14 00:13:27 +0000

    deleted the imports

--- warmup169/wsgi.py
+++ warmup169/wsgi.py
@@ -16,7 +16,4 @@ application = get_wsgi_application()
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-import newrelic.agent
-newrelic.agent.initialize('newrelic.ini')
-
 application = Cling(get_wsgi_application())
