fd7de43e511c349597509b2707fafac43942967c
abhi kalakuntla <abhi.kalakuntla@gmail.com>
2014-06-14 00:03:21 +0000

    added the newrelic ini file

--- warmup169/wsgi.py
+++ warmup169/wsgi.py
@@ -16,4 +16,7 @@ application = get_wsgi_application()
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
+
 application = Cling(get_wsgi_application())
